63b268a4b9947b1ffc15d8fff49e9306c60db7f4
Brett Chalupa <brettchalupa@gmail.com>
2023-03-06 10:32:19 -0500

    chore: sound player for sfx that uses settings

--- smart_input/Player.gd
+++ smart_input/Player.gd
@@ -18,7 +18,7 @@ func _physics_process(delta):
 		var beam = weapon.instantiate()
 		beam.global_position = global_position
 		beam.fire(rotation)
-		$BlastSfx.play()
+		Sound.play_sfx($BlastSfx)
 		get_tree().get_root().add_child(beam)
 
 	move_and_slide()
---
+++ sound.gd
@@ -0,0 +1,10 @@
+extends Node
+
+## Music & sound effect player and manager
+
+func play_sfx(sfx):
+	if Global.play_sfx:
+		if sfx.has_method("play"):
+			sfx.play()
+		else:
+			print_debug("Failed to play sound effect %s, doesn't have play() method" % sfx)
