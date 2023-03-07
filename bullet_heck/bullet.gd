37b00e6869e27d19c6062fc8cb8491b4ce417a63
Brett Chalupa <brettchalupa@gmail.com>
2023-03-07 15:25:01 -0500

    feat(bullet_patterns): add it!
    as an isolated prep for Bullet Heck's enemies

--- bullet_heck/bullet.gd
+++ bullet_heck/bullet.gd
@@ -1,14 +1,44 @@
-extends RigidBody2D
+class_name Bullet extends RigidBody2D
 
-const SPEED = 600.0
-@onready var timer = $Timer
+const SPEED := 600.0
+@onready var timer := $Timer
+
+enum BulletMovement {
+	STRAIGHT,
+	WIGGLY,
+	ARC,
+}
+
+var movement:BulletMovement = BulletMovement.STRAIGHT
 
 func _process(_delta):
 	if timer.time_left / timer.wait_time < 0.25:
 		$Sprite2D.modulate.a = timer.time_left / timer.wait_time
 
+func _physics_process(delta: float) -> void:
+	match movement:
+		BulletMovement.STRAIGHT:
+			# move as normal with linear velocity
+			pass
+		BulletMovement.WIGGLY:
+			var mod = timer.time_left * 20
+			position += Vector2(sin(mod), cos(mod)) * 10
+		BulletMovement.ARC:
+			var mod = timer.time_left * 3
+			rotation += mod
+			set_linear_velocity_from_rotation()
+
 func _on_timer_timeout():
 	queue_free()
 
-func fire(angle: float) -> void:
-	linear_velocity = Vector2.UP.rotated(angle) * SPEED
+## angle in rads to fire
+## lifetime in secs to live
+func fire(angle: float, lifetime:float=2.0, bmovement: BulletMovement=BulletMovement.STRAIGHT) -> void:
+	movement = bmovement
+	rotation = angle
+	set_linear_velocity_from_rotation()
+	timer.wait_time = lifetime
+	timer.start()
+
+func set_linear_velocity_from_rotation():
+	linear_velocity = Vector2.UP.rotated(rotation) * SPEED
---
+++ bullet_heck/enemy.gd
@@ -0,0 +1,53 @@
+extends RigidBody2D
+
+enum FirePattern {
+	SINGLE,
+	DOUBLE,
+	SPREAD,
+	CIRCLE,
+}
+
+@export var fire_pattern:FirePattern = FirePattern.SINGLE
+@export var bullet_movement:Bullet.BulletMovement = Bullet.BulletMovement.STRAIGHT
+@export var fire_delay:float = 1
+@export var bullet_scene:PackedScene
+@onready var weapon_sfx := $WeaponSfx
+@onready var timer := $FireTimer
+
+func _ready() -> void:
+	timer.wait_time = fire_delay
+	timer.start()
+
+func _on_fire_timer_timeout() -> void:
+	match fire_pattern:
+		FirePattern.SINGLE:
+			fire_bullet(global_position, 0)
+			Sound.play_sfx(weapon_sfx)
+		FirePattern.DOUBLE:
+			var spacer = Vector2(18, 0)
+			fire_bullet(global_position + spacer, 0)
+			fire_bullet(global_position - spacer, 0)
+			Sound.play_sfx(weapon_sfx)
+		FirePattern.SPREAD:
+			fire_bullet(global_position, 0)
+			fire_bullet(global_position, -20)
+			fire_bullet(global_position, 20)
+			Sound.play_sfx(weapon_sfx)
+		FirePattern.CIRCLE:
+			var shots = 16
+			for n in shots:
+				fire_bullet(global_position, (360 / shots) * n)
+			Sound.play_sfx(weapon_sfx)
+
+func fire_bullet(pos: Vector2, angle_deg: float):
+	var bullet = bullet_scene.instantiate()
+	bullet.global_position = pos
+	get_tree().get_root().add_child(bullet)
+	bullet.fire(deg_to_rad(angle_deg), 1, bullet_movement)
+	return bullet
+
+func stop_firing():
+	timer.paused = true
+
+func resume_firing():
+	timer.paused = false
