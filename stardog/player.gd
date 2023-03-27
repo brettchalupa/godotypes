1f012c1a185348d081e43a7b1f78e90ccba5f98f
Brett Chalupa <brettchalupa@gmail.com>
2023-03-27 09:12:17 -0400

    feat(stardog): moving player ship & asteroids

---
+++ stardog/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+
+const xy_accel = 400.0
+const xy_speed = 20.0
+var forward_speed = 20.0
+var forward_paused = false
+
+func _physics_process(delta: float) -> void:
+	var input_dir := Input.get_vector("ui_right", "ui_left", "ui_down", "ui_up")
+	var direction := (transform.basis * Vector3(input_dir.x, input_dir.y, 0)).normalized()
+	if direction:
+		velocity.x = move_toward(velocity.x, direction.x * xy_speed, xy_accel * delta)
+		velocity.y = move_toward(velocity.y, direction.y * xy_speed, xy_accel * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, xy_speed * delta * 2)
+		velocity.y = move_toward(velocity.y, 0, xy_speed * delta * 2)
+	
+	if !forward_paused:
+		velocity.z = forward_speed
+	else:
+		velocity.z = 0.0
+
+	move_and_slide()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("stardog_debug_toggle_player_z"):
+		forward_paused = !forward_paused
--- stardog/stardog.gd
+++ stardog/stardog.gd
@@ -1,4 +1,8 @@
 extends Node3D
 
 func _ready() -> void:
-	$MainMenuButton.connect("pressed", Global._on_main_menu_button_pressed)
+	$UI/MainMenuButton.connect("pressed", Global._on_main_menu_button_pressed)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug_reset_scene"):
+		get_tree().reload_current_scene.call_deferred()
