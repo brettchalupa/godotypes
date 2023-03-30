3bcdb128bc16164c25668e54d556355a05c91929
Brett Chalupa <brettchalupa@gmail.com>
2023-03-30 09:58:57 -0400

    feat(driftmania): moveable car & balls
    you can drive the car around, brake, idk maybe drift, push balls around!
    not 100% happy with this but I'm satisfied for now
    
    h/t to https://www.youtube.com/watch?v=zXLpitpFC6E ! so helpful

--- driftmania/driftmania.gd
+++ driftmania/driftmania.gd
@@ -2,3 +2,7 @@ extends Node3D
 
 func _ready() -> void:
 	$UI/MainMenuButton.connect("pressed", Global._on_main_menu_button_pressed)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("debug_reset_scene"):
+		get_tree().reload_current_scene.call_deferred()
--- driftmania/vehicle.gd
+++ driftmania/vehicle.gd
@@ -1,4 +1,25 @@
 extends VehicleBody3D
 
+@export var max_rpm := 800.0
+@export var max_torque := 200.0
+@export var break_force := 200.0
+@export var drift_force := 20.0
+@export var steer_force := 0.4
+
+@onready var wheel_rear_left = $"Wheel-RearLeft"
+@onready var wheel_rear_right = $"Wheel-RearRight"
+
 func _physics_process(delta: float) -> void:
-	var input_dir := Input.get_vector("ui_right", "ui_left", "ui_down", "ui_up")
+	steering = lerp(steering, Input.get_axis("ui_right", "ui_left")  * steer_force, 5 * delta)
+	var accel = Input.get_axis("ui_up", "ui_down")
+	var rpm = abs(wheel_rear_left.get_rpm())
+	wheel_rear_left.engine_force = accel * max_torque * (1 - rpm / max_rpm)
+	rpm = abs(wheel_rear_right.get_rpm())
+	wheel_rear_right.engine_force = accel * max_torque * (1 - rpm / max_rpm)
+
+	if Input.is_action_pressed("ui_accept"):
+		brake = break_force
+	elif Input.is_action_pressed("ui_cancel"):
+		brake = drift_force
+	else:
+		brake = 0
