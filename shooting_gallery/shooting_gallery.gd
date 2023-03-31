f51d1fbe586f08595ca57ed7b4812d242614b192
Brett Chalupa <brettchalupa@gmail.com>
2023-03-31 05:38:30 -0400

    feat(sg): init + fix main menu controls

---
+++ shooting_gallery/shooting_gallery.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+func _ready() -> void:
+	$UI/MainMenuButton.connect("pressed", Global._on_main_menu_button_pressed)
+
+func _input(event: InputEvent) -> void:
+	if OS.is_debug_build() && event.is_action_pressed("debug_reset_scene"):
+		get_tree().reload_current_scene.call_deferred()
