f9a661ffc985ca6e8cdac8b94a161f45b8f84258
Brett Chalupa <brettchalupa@gmail.com>
2023-04-01 07:55:46 -0400

    adjust(sg): properly set/reset mouse capture mode

---
+++ main_menu/main_menu.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
--- shooting_gallery/shooting_gallery.gd
+++ shooting_gallery/shooting_gallery.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$UI/MainMenuButton.connect("pressed", Global._on_main_menu_button_pressed)
 
 func _input(event: InputEvent) -> void:
